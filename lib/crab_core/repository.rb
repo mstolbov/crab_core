8f806fce4cd8d0221ae247952b0a29fe2443b9d8
Mikhail Stolbov <mstolbov@gmail.com>
2017-04-01 14:53:00 +0300

    rallback db connection

--- lib/crab_core/repository.rb
+++ lib/crab_core/repository.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 require 'yaml'
 require 'sequel'
-require 'logger'
 
 class CrabCore
   class ConfigurationError < CrabCoreError; end
@@ -9,8 +8,9 @@ class CrabCore
   class Repository
     class << self
       def connection
+        user = [ config['username'], config['password'] ].compact.join(':')
         @connection ||= Sequel.connect(
-          "postgres://iwywirpvrutazo:630db5fd6c47cac81cc40a59d455517acfd150976b53ea2d7517d3d456a876b5@ec2-184-72-216-69.compute-1.amazonaws.com:5432/d89m2tqnmgkrrh")
+          "#{config['adapter']}://#{user}@#{config['host']}:#{config['port']}/#{config['database']}")
       end
 
       def connection_without_database
